040a7fa5b8c7e0d6161e2011944e46a81ea5fe17
guyeinhoren <guyeinhoren@gmail.com>
2026-07-04 08:07:39 +0300

    Initial Commit

---
+++ ironGameDomeTests/ironGameDomeTests.swift
@@ -0,0 +1,19 @@
+//
+//  ironGameDomeTests.swift
+//  ironGameDomeTests
+//
+//  Created by גיא אינהורן on 04/07/2026.
+//
+
+import Testing
+@testable import ironGameDome
+
+struct ironGameDomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
